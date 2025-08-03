470b54a9344cab77bb356937c147ccb5b8037f76
Ristar-art <mochochokoboiketlo@gmail.com>
2025-08-03 02:43:15 +0200

    error fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Language" content="en" />
-    <html lang="en"></html>
     
     <!-- Canonical URL -->
     <link rel="canonical" href={pageUrl} />
